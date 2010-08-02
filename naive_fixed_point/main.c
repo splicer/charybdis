a408806213407a116743fbff8a0f5e5645002487
Eric Garber <ubernostrom@gmail.com>
2010-08-01 23:40:42 -0700

    Moved image array into header file.

--- naive_fixed_point/main.c
+++ naive_fixed_point/main.c
@@ -1,13 +1,8 @@
 #include <stdint.h>
 #include <stdio.h>
 
-#define IMG_ROWS    1
-#define IMG_COLS    4
-#define IMG_RGB     3
+#include "test_image.h"
 
-uint8_t image[IMG_ROWS][IMG_COLS][IMG_RGB] = {{
-                                                {200, 0, 2}, {127, 0, 0}, {155, 2, 0}, {140, 4, 2}
-                                            }};
 
 /* 8-bit */
 static inline int rgb2y(int r, int g, int b)
---
+++ naive_fixed_point/test_image.h
@@ -0,0 +1,8 @@
+#define IMG_ROWS    1
+#define IMG_COLS    4
+#define IMG_RGB     3
+
+uint8_t image[IMG_ROWS][IMG_COLS][IMG_RGB] = {
+                                                { {200, 0, 2}, {127, 0, 0}, {155, 2, 0}, {140, 4, 2} }
+                                            };
+
