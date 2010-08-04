613b4564594082ee5f3c06aea38b9ed4de29a4a3
Eric Garber <ubernostrom@gmail.com>
2010-08-03 23:22:25 -0700

    Commiting changes to generate_tables that should have been in commit 998d9ff.

--- with_hardware/generate_tables.py
+++ with_hardware/generate_tables.py
@@ -1,5 +1,7 @@
 #! /usr/bin/python2.6
 
+from bitstring import BitString
+
 c1 = 65.738 / 256
 c2 = 129.057 / 256
 c3 = 25.064 / 256
@@ -11,8 +13,8 @@ c8 = -94.154 / 256 * 2
 c9 = -18.285 / 256 * 2
 
 def binary(n, digits=8):
-    rep = bin(n)[2:]
-    return ('0' * (digits - len(rep))) + rep
+    b = BitString(int=n, length=digits)
+    return b.bin[2:] # chop off '0b' prefix 
 
 def print_lut(coeff, values_out, bits_out):
     for i in range(values_out):
@@ -21,5 +23,5 @@ def print_lut(coeff, values_out, bits_out):
             print ","
 
 if __name__ == '__main__':
-    print "Table for c1: \n"
-    print_lut(c1, 256, 8);
+    print "Table for c9: \n"
+    print_lut(c9, 256, 9);
