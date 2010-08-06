a70a4e9f2930a3b48af1d154473c211f5b696d7c
Andrew Garber <andrew@andrewgarber.com>
2010-08-05 22:09:24 -0700

    test script now works!

--- test_naive_fixed_point.py
+++ test_naive_fixed_point.py
@@ -3,16 +3,17 @@
 from floating_point import downsampled_ycc_from_rgb
 import random
 import subprocess
+import re
 
 def run_naive_fixed_point(test_vector):
-    args = [item for sublist in test_vector for item in sublist]
-    #args = " ".join([str(i) for i in args])
-    #os.system("echo '%s' | ./naive_fixed_point/unix/test" % args)
-    print subprocess.check_output(["./naive_fixed_point/unix/test"] + args)
+    args = [str(item) for sublist in test_vector for item in sublist]
+    result = subprocess.Popen(["naive_fixed_point/unix/test"] + args, stdout=subprocess.PIPE).communicate()[0]
+    return [int(i) for i in re.split(r"\s+", result.strip())]
 
 random.seed()
 for i in xrange(10):
     test_vector = [[random.randint(0, 255) for x in range(3)] for y in range(4)]
-    #print test_vector
-    run_naive_fixed_point(test_vector)
-    #print downsampled_ycc_from_rgb(*test_vector)
+    #test_vector = [[255,255,255],[0,0,0],[0,0,0],[0,0,0]]
+    print test_vector
+    print run_naive_fixed_point(test_vector)
+    print downsampled_ycc_from_rgb(*test_vector)
