07a9bc81e1533689a22b25a027f0541884f4ecc5
Andrew Garber <andrew@andrewgarber.com>
2010-08-06 02:12:05 -0700

    naive_fixed_point now uses 30-bit intermediate values for Cb and Cr
    This results in much more accuracy (possibly perfect). The largest value
    you can get when adding four 30-bit values together is a 32-bit value.

--- naive_fixed_point/common/rgb2y.c
+++ naive_fixed_point/common/rgb2y.c
@@ -19,41 +19,38 @@
  */
 
 /* 8-bit */
-static inline uint16_t rgb2y( uint8_t r, uint8_t g, uint8_t b )
+static inline uint8_t rgb2y( uint8_t r, uint8_t g, uint8_t b )
 {
-    // FP shift of 24
     uint32_t y = ( ( uint32_t )( ( 65.7377882352941 * ( 1<<16 ) ) + 0.5 ) * r ) +
         ( ( uint32_t )( ( 129.0571294117647 * ( 1<<16 ) ) + 0.5 ) * g ) +
         ( (uint32_t )( ( 25.0639058823529 * ( 1<<16 ) ) + 0.5 ) * b ) +
         ( 256 * ( 1<<16 ) ) * 16;
     y = ( y + ( 1<<23 ) ) >> 24;
-    return ( uint16_t )y;
+    return ( uint8_t )y;
 }
 
 
-/* 9-bit */
-static inline uint16_t rgb2cb( uint8_t r, uint8_t g, uint8_t b )
+/* 30-bit */
+static inline uint32_t rgb2cb( uint8_t r, uint8_t g, uint8_t b )
 {
-    // FP shift of 22
     int32_t cb = ( ( int32_t )( ( -37.9450869960784 * ( 1<<15 ) ) - 0.5 ) * r ) +
         ( ( int32_t )( ( -74.4941286901961 * ( 1<<15 ) ) - 0.5 ) * g ) +
         ( ( int32_t )( ( 112.4392156862745 * ( 1<<15 ) ) + 0.5 ) * b ) +
         ( 256 * ( 1<<15 ) ) * 128;
-    cb = ( cb + ( 1<<21 ) ) >> 22;
-    return ( uint16_t )cb;
+    cb = ( cb + 1 ) >> 1;
+    return ( uint32_t )cb;
 }
 
 
-/* 9-bit */
-static inline uint16_t rgb2cr( uint8_t r, uint8_t g, uint8_t b )
+/* 30-bit */
+static inline uint32_t rgb2cr( uint8_t r, uint8_t g, uint8_t b )
 {
-    // FP shift of 22
     int32_t cr = ( ( int32_t )( ( 112.4392156862745 * ( 1<<15 ) ) + 0.5 ) * r ) +
         ( ( int32_t )( ( -94.1539006745098 * ( 1<<15 ) ) - 0.5 ) * g ) +
         ( ( int32_t )( ( -18.2853150117647 * ( 1<<15 ) ) - 0.5 ) * b ) +
         ( 256 * ( 1<<15 ) ) * 128;
-    cr = ( cr + ( 1<<21 ) ) >> 22;
-    return ( uint16_t )cr;
+    cr = ( cr + 1 ) >> 1;
+    return ( uint32_t )cr;
 }
 
 
@@ -69,27 +66,27 @@ int rgb2y_convertImage( const rgb2y_rgbPixel_t *rgbImg,
             const rgb2y_rgbPixel_t *p = &rgbImg[i * width + j];
 
             yQuad->y[0] = rgb2y( p->r, p->g, p->b );
-            uint16_t cb1 = rgb2cb( p->r, p->g, p->b );
-            uint16_t cr1 = rgb2cr( p->r, p->g, p->b );
+            uint32_t cb1 = rgb2cb( p->r, p->g, p->b );
+            uint32_t cr1 = rgb2cr( p->r, p->g, p->b );
             p++;
 
             yQuad->y[1] = rgb2y( p->r, p->g, p->b );
-            uint16_t cb2 = rgb2cb( p->r, p->g, p->b );
-            uint16_t cr2 = rgb2cr( p->r, p->g, p->b );
+            uint32_t cb2 = rgb2cb( p->r, p->g, p->b );
+            uint32_t cr2 = rgb2cr( p->r, p->g, p->b );
             p = &rgbImg[(i+1) * width + j];
 
             yQuad->y[2] = rgb2y( p->r, p->g, p->b );
-            uint16_t cb3 = rgb2cb( p->r, p->g, p->b );
-            uint16_t cr3 = rgb2cr( p->r, p->g, p->b );
+            uint32_t cb3 = rgb2cb( p->r, p->g, p->b );
+            uint32_t cr3 = rgb2cr( p->r, p->g, p->b );
             p++;
 
             yQuad->y[3] = rgb2y( p->r, p->g, p->b );
-            uint16_t cb4 = rgb2cb( p->r, p->g, p->b );
-            uint16_t cr4 = rgb2cr( p->r, p->g, p->b );
+            uint32_t cb4 = rgb2cb( p->r, p->g, p->b );
+            uint32_t cr4 = rgb2cr( p->r, p->g, p->b );
 
-            // downsample 4 9-bit values to 1 8-bit value for Cb and Cr
-            yQuad->cb = ( ( cb1 + cb2 + cb3 + cb4 ) + ( 1<<2 ) ) >> 3;
-            yQuad->cr = ( ( cr1 + cr2 + cr3 + cr4 ) + ( 1<<2 ) ) >> 3;
+            // downsample 4 30-bit values to 1 8-bit value for Cb and Cr
+            yQuad->cb = ( ( cb1 + cb2 + cb3 + cb4 ) + ( 1<<23 ) ) >> 24;
+            yQuad->cr = ( ( cr1 + cr2 + cr3 + cr4 ) + ( 1<<23 ) ) >> 24;
 
             yQuad++;
         }
